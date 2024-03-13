722e41c36ec78adb29813984ccc6ca5c109dd4a9
Poccers <jaha0004@stud.kea.dk>
2024-03-14 00:46:47 +0100

    Merge remote-tracking branch 'origin/main'

--- Madspildwebsite/src/main/java/com/example/madspildwebsite/controller/HomeController.java
+++ Madspildwebsite/src/main/java/com/example/madspildwebsite/controller/HomeController.java
@@ -12,6 +12,16 @@ public class HomeController {
     public String forbruger(){
         return "forbruger";
     }
+    @GetMapping("/organisationerforside")
+    public String OrganisationerForside() {return "OrganisationerForside";}
+    @GetMapping("/SSL")
+    public String ssl() {return "SSL";}
+    @GetMapping("/TGTG")
+    public String tgtg() {return "TGTG";}
+    @GetMapping("/MNSTRT")
+    public String mnstrt() {return "ministeriet";}
+    @GetMapping("/LNDBRG")
+    public String lndbrg() {return "landbrug";}
     @GetMapping("/header")
     public String header(){
         return "header";
